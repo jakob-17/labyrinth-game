4ad8d520f00c0a3861a2070ff24615ea297da43c
Jakob Horvath <horvath.j17@gmail.com>
2019-09-01 21:57:58 -0700

    broken enemy script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -22,10 +22,19 @@ public class Enemy : MonoBehaviour
         rb2d.velocity = currentDir * speed;
     }
 
+    private float interval = 1.0f;
+
     // Update is called once per frame
     void Update()
     {
-        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, currentDir);
+        if (Time.time >= interval)
+        {
+            interval++;
+
+            UpdateEverySecond();
+        }
+
+/*        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, currentDir);
         if (hit.collider != null && hit.distance < 0.001f)
         {
             rb2d.velocity = Vector2.zero;
@@ -36,6 +45,62 @@ public class Enemy : MonoBehaviour
             do { index = UnityEngine.Random.Range(0, 4); } while (dirs[index] == currentDir);
             currentDir = dirs[index];
             rb2d.velocity = currentDir * speed;
+        }*/
+    }
+
+    // name seems self explanatory
+    void UpdateEverySecond()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, currentDir);
+
+        if (hit.collider != null && hit.distance < 0.1f)
+        {
+            int index;
+            RaycastHit2D diffDir;
+            if (dirs[0] == currentDir) // moving up
+            {
+                do
+                {
+                    index = UnityEngine.Random.Range(1, 4);
+                    diffDir = Physics2D.Raycast(this.transform.position, dirs[index]);
+                } while (diffDir.distance < 0.5f);
+
+                currentDir = dirs[index];
+                rb2d.velocity = currentDir * speed;
+            }
+            else if (dirs[1] == currentDir) // moving down
+            {
+                do
+                {
+                    index = UnityEngine.Random.Range(0, 4);
+                    diffDir = (index != 1) ? Physics2D.Raycast(this.transform.position, dirs[index]) : Physics2D.Raycast(this.transform.position, dirs[0]);
+                } while (diffDir.distance < 0.5f);
+
+                currentDir = dirs[index];
+                rb2d.velocity = currentDir * speed;
+            }
+            else if (dirs[2] == currentDir) // moving right
+            {
+                do
+                {
+                    index = UnityEngine.Random.Range(0, 4);
+                    diffDir = (index != 2) ? Physics2D.Raycast(this.transform.position, dirs[index]) : Physics2D.Raycast(this.transform.position, dirs[3]);
+                } while (diffDir.distance < 0.5f);
+
+                currentDir = dirs[index];
+                rb2d.velocity = currentDir * speed;
+            }
+            else if (dirs[3] == currentDir) // moving left
+            {
+                do
+                {
+                    index = UnityEngine.Random.Range(0, 3);
+                    diffDir = Physics2D.Raycast(this.transform.position, dirs[index]);
+                } while (diffDir.distance < 0.5f);
+
+                currentDir = dirs[index];
+                rb2d.velocity = currentDir * speed;
+            }
         }
     }
 }
